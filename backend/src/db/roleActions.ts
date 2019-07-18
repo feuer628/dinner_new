2f815808e76d445ebd3b6241013888c18954cc6e
silakov <silakov@bifit.com>
2019-07-18 17:17:46 +0300

    Роли и права доступа. enum c правами доступа.

---
+++ backend/src/db/roleActions.ts
@@ -0,0 +1,9 @@
+export enum RoleActions {
+    SYSTEM_ADMIN = 1,
+    ORDER_FOR_OTHER = 2,
+    ADD_ORG = 3,
+    MANAGE_ORG_GROUP = 4,
+    CONFIRM_ALL_USERS = 5,
+    CONFIRM_ORG_USERS = 6,
+    MANAGE_RIGHTS_ORG = 7
+}
