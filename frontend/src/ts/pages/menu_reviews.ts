1e2640972178bbc80a99b72b2dd1f3c738a1016f
silakov <silakov@bifit.com>
2019-07-24 16:14:37 +0300

    bugfixes

--- frontend/src/ts/pages/menu_reviews.ts
+++ frontend/src/ts/pages/menu_reviews.ts
@@ -12,7 +12,7 @@ import {UI} from "../components/ui";
 <div id="reviews">
     <h4 class="alignC">Отзывы о блюдах</h4>
     <b-card-group columns>
-        <b-card v-for="item in reviews" :header-bg-variant="headerVariant(item)" :header="item.menu_item_name" class="reviewCard">
+        <b-card v-for="item in reviews" :key="item.id" :header-bg-variant="headerVariant(item)" :header="item.menu_item_name" class="reviewCard">
             <b-card-text>{{item.review}}</b-card-text>
             <div slot="footer"><b-form-input v-model="item.rating" :class="headerVariant(item)" type="range" min="0" max="10"></b-form-input></div>
         </b-card>
@@ -24,20 +24,6 @@ export class MenuReviews extends UI {
 
     /** Отзывы о блюдах */
     private reviews: MenuItemReview[] = [];
-
-    /** Описание полей таблицы */
-    private orderFields = {
-        menu_item_name: {
-            label: "Название блюда"
-        },
-        review: {
-            label: "Отзыв"
-        },
-        rating: {
-            key: "rating",
-            label: "Рейтинг"
-        }
-    };
 
     private async mounted(): Promise<void> {
         this.reviews = await this.rest.loadItems<MenuItemReview>("users/reviews");
